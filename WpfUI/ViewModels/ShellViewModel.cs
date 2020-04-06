82c728a9f02a61e1d19cda833d14b4468627db91
Jani Pehkonen <janpe2@users.noreply.github.com>
2020-04-06 11:32:52 +0300

    Add project files.

---
+++ WpfUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace WpfUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+        public void LoadDish()
+        {
+            MessageBox.Show("Abc");
+        }
+    }
+}
